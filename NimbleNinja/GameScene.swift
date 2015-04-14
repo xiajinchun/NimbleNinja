35bfbd5c5c113b071a15beddb56850cb9644d76d
xiajinchun <xiajinchunc@gmail.com>
2015-04-14 13:21:14 +0800

    Initial Commit

---
+++ NimbleNinja/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  NimbleNinja
+//
+//  Created by  夏锦春 on 15/4/14.
+//  Copyright (c) 2015 TEAM. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
